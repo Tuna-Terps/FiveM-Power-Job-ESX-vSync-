f0ecc00d555fdba100e78283ac7d65b30107e436
Tuna Terps <62733984+Tuna-Terps@users.noreply.github.com>
2021-07-09 06:31:15 -0400

    Add files via upload

---
+++ TunasPowerJob/fxmanifest.lua
@@ -0,0 +1,18 @@
+fx_version 'cerulean'
+resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
+games { 'rdr3', 'gta5' }
+
+author 'Tuna Terps'
+description 'No POWER ? No lights !! An ESX job that requires players to maintain a power grid for the city !'
+version '1.0.0'
+
+client_script 'client/job.lua'
+
+server_scripts {
+    'server/grid.js',
+    'server/payment.lua'
+}
+
+dependencies {
+    "vSync"
+}
